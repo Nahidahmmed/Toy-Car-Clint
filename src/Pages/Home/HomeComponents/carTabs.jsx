c80d177b60aaf66fe4baae3154efd2ce6c4085d3
Nahidahmmed <nahidahmmed411@gmail.com>
2023-05-19 15:34:59 +0600

    tab card done

--- src/Pages/Home/HomeComponents/carTabs.jsx
+++ src/Pages/Home/HomeComponents/carTabs.jsx
@@ -27,7 +27,7 @@ const CarTabs = () => {
         <div className=" lg:ml-5 lg:mr-5 mt-24 ">
         <Tabs selectedIndex={activeTab} onSelect={handleTabChange}>
           <TabList>
-            <div className=" space-y-5 lg:flex space-x-5">
+            <div className=" space-y-5 lg:flex space-x-52 items-end">
             {categories.map((category, index) => (
               <Tab key={index}><button className="btn">{category.name}</button></Tab>
             ))}
@@ -36,14 +36,16 @@ const CarTabs = () => {
   
           {categories.map((category, index) => (
             <TabPanel key={index}>
-              <div className="subCategories w-6/12 lg:grid grid-cols-2 gap-4 ">
+              <div className="subCategories mt-8 h-[700px]  lg:grid grid-cols-2 gap-4 mx-auto">
                 {category.toys.map((toy, index) => (
-                  <div key={index} className="mx-auto">
-                    <img className="w-72 h-2/4" src={toy.image} alt={toy.name} />
+                  <div key={index} className="mx-auto card-body mt-9 h-[500px] border ">
+                    <img className="w-72 h-64" src={toy.image} alt={toy.name} />
+                    <div className="space-y-3 text-xl font-bold">
                     <h3>{toy.name}</h3>
                     <p>{toy.price}</p>
                     <p>Rating: {toy.rating}</p>
                     <button className="btn btn-primary">{toy.details}</button>
+                    </div>
                   </div>
                 ))}
               </div>
